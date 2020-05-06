7f0d9b2ba8bc189b62263c278cc4422a12f66cef
younghcan <kimynam1@naver.com>
2020-05-06 17:58:00 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -11,7 +13,7 @@ const userSchema = mongoose.Schema({
         unique:1,
     },
     password:{
-        type:String,
+        type:String, 
         minlength:5,
     },
     lastname:{
@@ -31,6 +33,20 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save',function(next){
+    let user = this;
+    //비밀번호를 암호화 시킨다.
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password ,salt , function(err , hash){
+                if(err) return next(err)
+                user.password = hash
+                next();
+            });
+        });
+    }
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User}
