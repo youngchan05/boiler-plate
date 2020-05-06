a0c9e8ba591f90d60a6480590d121161640c63fb
younghcan <kimynam1@naver.com>
2020-05-06 16:50:06 +0900

    postMan test

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const {User} = require('./models/User'); 
 
+//application/x=www=form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//apllication/json
+app.use(bodyParser.json());
+console.log(app.use(bodyParser.json()))
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://youngchan:cksl6814@boilerplate-awtvp.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -10,6 +19,20 @@ mongoose.connect('mongodb+srv://youngchan:cksl6814@boilerplate-awtvp.mongodb.net
     useFindAndModify:false, //에러방지
 }).then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요1111'))
+
+app.post('/register',(req ,res)=> {
+    //회원가입 할때 필요한 정보들을 client 에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body)
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false , err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/User.js
+++ models/User.js
@@ -31,5 +31,6 @@ const userSchema = mongoose.Schema({
     }
 })
 
+
 const User = mongoose.model('User', userSchema);
 module.exports = {User}
