6ca6df25bb91653c7e20192ca8839d96e9757274
younghcan <kimynam1@naver.com>
2020-06-02 16:33:46 +0900

    디렉토리 구조 설정

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react'
+import axios from 'axios'
+
+function LandingPage() {
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response))
+    }, [])
+    return (
+        <div>
+            LendingPage  
+        </div>
+    )
+}
+
+export default LandingPage
--- server/index.js
+++ server/index.js
@@ -23,7 +23,9 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!~~ '))
 
-
+app.get('/api/hello',(req , res) =>{
+  res.send("안녕하세요");
+})
 app.post('/api/users/register', (req, res) => {
 
   //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
