5d88c310c326097f712c381b9056d2529beec13f
younghcan <kimynam1@naver.com>
2020-06-02 17:52:49 +0900

    redux 적용중

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
+import { applyMiddleware , createStore} from 'redux';
+import promisMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer  from './_reducers';
 
+const createStoreWithMiddleware = applyMiddleware(promisMiddleware ,ReduxThunk )(createStore);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+  >
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
