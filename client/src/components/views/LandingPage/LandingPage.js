d02e8950c0c78065d64367b4c28f97ea1d59192c
younghcan <kimynam1@naver.com>
2020-06-05 13:42:09 +0900

    login redux 적용

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,8 +7,10 @@ function LandingPage() {
         .then(response => console.log(response))
     }, [])
     return (
-        <div>
-            LendingPage  
+        <div style={{
+            display:'flex', justifyContent:'center' , alignItems:'center', width:'100%', height:'100vh'
+        }}>
+            <h2>시작페이지</h2>
         </div>
     )
 }
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,48 @@
-import React from 'react'
-
-function LoginPage() {
+import React , {useState} from 'react'
+import axios from 'axios';
+import {useDispatch} from 'react-redux';
+import {LoginUser} from '../../../_actions/User_action'
+function LoginPage(props) {
+    const dispatch = useDispatch();
+    const  [Email , setEmail] = useState("")
+    const  [Password , setPassword] = useState("")
+    const onEmailHendler = (event) => {
+        setEmail(event.target.value);
+    }
+    const onPassword = (event) => {
+        setPassword(event.target.value)
+    }
+    const onSubmitHendler = (event) =>{
+        event.preventDefault();
+        let body = {
+            email : Email,
+            password : Password
+        }
+        dispatch(LoginUser(body))
+        .then(response => {
+            if(response.payload.loginSuccess){
+                props.history.push('/');
+            }else {
+                alert('Error');
+            }
+        })
+    }   
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display:'flex', justifyContent:'center' , alignItems:'center', width:'100%', height:'100vh'
+        }}>
+            <form style={{ display:'flex', flexDirection:'column'}}
+                onSubmit={onSubmitHendler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHendler}/>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPassword}/>
+                <br/>
+                <button>
+                    login
+                </button>
+            </form>
         </div>
     )
 }
